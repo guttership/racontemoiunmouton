28026744610a5bb619aaabe1c6214067ef30df57
guttership <guttership@users.noreply.github.com>
2025-10-15 12:45:55 +0200

    feat(analytics): ajout de Google Analytics (GA4)
    - ID de mesure: G-MW948L3Z01
    - Utilisation du composant Script Next.js
    - Strategy afterInteractive pour ne pas bloquer l'hydratation
    - Intégration avec Consent Manager pour le RGPD

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,25 @@ export default async function RootLayout({
           data-cmp-cdn="cdn.consentmanager.net"
           data-cmp-codesrc="16"
         />
+        
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-MW948L3Z01"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-MW948L3Z01');
+            `,
+          }}
+        />
+        
         {children}
       </body>
     </html>
